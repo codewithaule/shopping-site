47481db0d36fb93d9eecbaec2d337e9127fcb114
codewithaule <94807384+codewithaule@users.noreply.github.com>
2022-03-02 04:58:14 +0100

    Add files via upload

---
+++ src/views/Checkout.vue
@@ -0,0 +1,67 @@
+<template>
+   <div class="page-checkout">
+       <div class="columns is-multiline">
+           <div class="column is-12">
+               <h1 class="title">Checkout</h1>
+           </div>
+           <div class="column is-12 box"> 
+               <h2 class="subtitle">Payment</h2>
+               <div class="columns is-multiline">
+                   <div class="column is-6">
+                       <div class="field">
+                           <label> Amount*</label>
+                           <div class="control">
+                               <input type="amount" class="input" v-model="amount">
+                           </div>
+                       </div>
+                       <div class="field">
+                           <label> E-mail*</label>
+                           <div class="control">
+                               <input type="email" class="input" v-model="email">
+                           </div>
+                       </div>
+                    </div>
+                     <hr>
+                   
+                        <button class="button is-dark" @click="submitForm()" type="submit"> make payment </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import VuePaystack from "vue-paystack2"
+
+export default {
+  name: 'Checkout',
+   
+  data () {
+    return {
+        paystackKey: {},
+        email: '',
+        amount: 0,
+        reference: ''
+        
+    }
+  },
+  methods: {
+      submitForm() {
+          const data = {
+              currency: "NGN",
+              plan :"",
+              ref:"{{ payment.ref}}",
+              key: "{{ paystack_public_key}}",
+              email: '{{ payment.email}}',
+              amount: '{{ payment.amount_value }}',
+              
+             } 
+            
+
+      }
+    
+       
+    }
+}
+</script>
---
+++ src/views/Success.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="page-Success">
+       <div class="columns is-multiline">
+           <div class="column is-12">
+               <h1 class="title">Thank you</h1>
+
+               <p> Your order will be processes within 48 hours </p>
+           </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Success',
+    mounted() {
+        document.title = 'Success | shirts '
+    },
+
+    
+}
+</script>
